2039a50f9da25b04455994d79ab7cc14ad371860
otienochris <otienochris98@gmail.com>
2022-04-15 00:48:18 +0300

    adding lerning page for a course'

--- src/pages/student-interface-page/MyCourses.jsx
+++ src/pages/student-interface-page/MyCourses.jsx
@@ -3,13 +3,17 @@ import { Box, Grid } from '@mui/material';
 import React, { useState } from 'react';
 import CourseInProgressPreview from '../../components/CourseInProgressPreview';
 
-export default function MyCourses({ courses, setContinueLearning }) {
+export default function MyCourses({
+  courses,
+  setContinueLearning,
+  setCourseToContinue,
+}) {
   return (
     <>
       <Typography variant='h6'>My Courses</Typography>
       <Grid container justifyContent={'center'}>
-        {courses.map((course) => (
-          <Grid item xs={12} sm={8} md={4}>
+        {courses.map((course, index) => (
+          <Grid item xs={12} sm={8} md={4} key={index}>
             <Box
               sx={{
                 '&:hover': {
@@ -20,6 +24,7 @@ export default function MyCourses({ courses, setContinueLearning }) {
               <CourseInProgressPreview
                 course={course}
                 setContinueLearning={setContinueLearning}
+                setCourseToContinue={setCourseToContinue}
               />
             </Box>
           </Grid>
