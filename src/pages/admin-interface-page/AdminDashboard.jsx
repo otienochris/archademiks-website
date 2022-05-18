a12d0330153b810f045b2db3fd40cd059eb8ca19
otienochris <otienochris98@gmail.com>
2022-05-18 21:32:16 +0300

    adding conditional rendering in admin panel

--- src/pages/admin-interface-page/AdminDashboard.jsx
+++ src/pages/admin-interface-page/AdminDashboard.jsx
@@ -9,6 +9,7 @@ import FinancialQuickStat from './FinancialQuickStat';
 import ChartComponent from './ChartComponent';
 import { userPerMonth } from '../../data/reportData';
 import { salesPerMonth } from '../../data/reportData';
+import CustomMaterialTable from '../../components/CustomMaterialTable';
 
 const useStyles = makeStyles({
   mainGrid: {
@@ -26,6 +27,17 @@ function AdminDashboard() {
   const [tutors, setTutors] = useState(
     users.filter((user) => user.type == 'instructor')
   );
+
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  });
 
   return (
     <Grid container alignContent='center' alignItems='center'>
@@ -48,22 +60,66 @@ function AdminDashboard() {
         </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <ChartComponent
-          title={'Sales per year: '}
-          data={salesPerMonth}
-          xaxisKey={'month'}
-          lineOneKey={'amount'}
-        />
+      <Grid item xs={12} sm={0} lg={0}>
+        {/* <Typography>Go to larger screen to view the charts</Typography> */}
       </Grid>
-      <Grid item xs={12}>
-        <ChartComponent
-          title={'User enrollments per year: '}
-          data={userPerMonth}
-          xaxisKey={'month'}
-          lineOneKey={'students'}
-          lineTwoKey={'instructors'}
-        />
+      <Grid item xs={12} sm={0}></Grid>
+
+      <Grid item sm={12}>
+        {isDesktop ? (
+          <ChartComponent
+            title={'Sales per year: '}
+            data={salesPerMonth}
+            xaxisKey={'month'}
+            lineOneKey={'amount'}
+          />
+        ) : (
+          <CustomMaterialTable
+            style={{ margin: '10px auto' }}
+            title={'Sales'}
+            data={salesPerMonth.flatMap((item) => {
+              const sum = item.data
+                .flatMap((item2) => item2.amount)
+                .reduce((prev, curr) => prev + curr);
+              return { year: item.year, amount: sum };
+            })}
+            columns={[
+              { title: 'Year', field: 'year' },
+              { title: 'Total Amount', field: 'amount' },
+            ]}
+            allowActions={false}
+          />
+        )}
+      </Grid>
+      <Grid item sm={12}>
+        {isDesktop ? (
+          <ChartComponent
+            title={'User enrollments per year: '}
+            data={userPerMonth}
+            xaxisKey={'month'}
+            lineOneKey={'students'}
+            lineTwoKey={'instructors'}
+          />
+        ) : (
+          <CustomMaterialTable
+            style={{ margin: '10px auto' }}
+            title={'Users'}
+            data={userPerMonth.flatMap((item) => {
+              const instructorsNo = item.data
+                .flatMap((item2) => item2.instructors)
+                .reduce((prev, curr) => prev + curr);
+              const studentsNo = item.data
+                .flatMap((item2) => item2.students)
+                .reduce((prev, curr) => prev + curr);
+              return { year: item.year, noOfUsers: instructorsNo + studentsNo };
+            })}
+            columns={[
+              { title: 'Year', field: 'year' },
+              { title: 'Enrolled Users', field: 'noOfUsers' },
+            ]}
+            allowActions={false}
+          />
+        )}
       </Grid>
 
       <Grid item xs={12}></Grid>
