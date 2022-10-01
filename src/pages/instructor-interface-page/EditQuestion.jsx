3b4e9b65f42dc06dc466469c815cd9a5332b0a11
otienochris <otienochris98@gmail.com>
2022-10-01 10:17:48 +0300

    adding question level and answer type indicators

--- src/pages/instructor-interface-page/EditQuestion.jsx
+++ src/pages/instructor-interface-page/EditQuestion.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import EditAnswers from './EditAnswers';
 
@@ -7,9 +7,65 @@ function EditQuestion({ question, addAnswerPayload }) {
 
   return (
     <>
+      <Grid container>
+        <Grid item xs={12} sm={'auto'}>
+          <Typography
+            align='center'
+            style={
+              question.level === 'EASY'
+                ? {
+                    backgroundColor: '#0CF574',
+                    border: '2px solid #0CF574',
+                    borderRadius: '10px',
+                    color: 'black',
+                    fontWeight: 'bolder',
+                    margin: '10px',
+                    padding: '10px',
+                  }
+                : question.level === 'MODERATE'
+                ? {
+                    backgroundColor: '#FFD166',
+                    border: '2px solid #FFD166',
+                    borderRadius: '10px',
+                    color: 'black',
+                    fontWeight: 'bolder',
+                    margin: '10px',
+                    padding: '10px',
+                  }
+                : {
+                    backgroundColor: '#F71735',
+                    border: '2px solid #F71735',
+                    borderRadius: '10px',
+                    color: 'black',
+                    fontWeight: 'bolder',
+                    margin: '10px',
+                    padding: '10px',
+                  }
+            }
+          >
+            {question.level}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={'auto'}>
+          <Typography
+            align='center'
+            style={{
+              border: '2px solid black',
+              borderRadius: '10px',
+              color: 'black',
+              margin: '10px',
+              padding: '10px',
+              fontWeight: 'bolder',
+            }}
+          >
+            {question.answerType}
+          </Typography>
+        </Grid>
+      </Grid>
       <Typography variant='body1' style={{ margin: '5px' }}>
         {question.question}
       </Typography>
+
       <EditAnswers
         answers={question.answers}
         addAnswerPayload={addAnswerPayload}
