96200ebd767d5eb1f878aad73f7f2e4b53d3ef2c
otienochris <otienochris98@gmail.com>
2023-01-29 09:03:58 +0300

    adding tutor preview in course preview

--- src/components/InstructorPreview.jsx
+++ src/components/InstructorPreview.jsx
@@ -4,123 +4,78 @@ import {
   CardActions,
   CardContent,
   CardHeader,
-  Collapse,
-  Grid,
+  Divider,
   IconButton,
   Typography,
-  styled,
 } from '@material-ui/core';
-import React, { useState } from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React from 'react';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
 export default function InstructorPreview({ instructor }) {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Card>
       <CardHeader
+        style={{ borderBottom: '1px solid grey' }}
         avatar={
-          <Avatar>{`${
-            instructor.firstName[0] + instructor.lastName[0]
-          }`}</Avatar>
+          <Avatar>{`${instructor.firstName[0] + instructor.lastName[0]
+            }`}</Avatar>
         }
         title={`${instructor.firstName + ' ' + instructor.lastName}`}
         subheader={instructor.title}
       />
       <CardContent>
-        <Typography>{instructor.briefDescription}</Typography>
+        <Typography>{instructor.description}</Typography>
       </CardContent>
 
-      <CardActions disableSpacing>
-        <IconButton
-          aria-label='whatsapp'
-          disabled={
-            instructor.contacts[3].url === '' ||
-            instructor.contacts[3].url === null
-              ? true
-              : false
-          }
-        >
-          <a
-            href={`https://api.whatsapp.com/send?phone=${instructor.contacts[3].url}`}
-            target='_blank'
-            rel='noopener noreferrer'
+      <CardActions style={{ display: 'block', backgroundColor: '#0D0221', padding: '10px auto' }}>
+        <Typography style={{ color: 'white' }}>Contact me through:</Typography>
+        <Divider component={'hr'} style={{ border: '1px solid white', margin: '5px auto 5px 0px', maxWidth: '150px' }} />
+        <div style={{ display: 'flex', width: '100%', margin: '10px auto' }}>
+
+
+          <IconButton
+            aria-label='whatsapp'
+            style={{ borderLeft: '1px solid yellowgreen', borderRight: '1px solid yellowgreen', borderRadius: '5px', padding: '5px', margin: 'auto 10px auto 0px' }}
           >
-            <WhatsAppIcon style={{ color: 'green' }} />
-          </a>
-        </IconButton>
-        <IconButton
-          aria-label='share'
-          disabled={
-            instructor.contacts[0].url === '' ||
-            instructor.contacts[0].url === null
-              ? true
-              : false
-          }
-        >
-          <a
-            href={instructor.contacts[0].url}
-            target='_blank'
-            rel='noopener noreferrer'
+            <a
+              href={instructor.contacts == undefined || instructor.contacts[3].url === null || instructor.contacts[3].url === '' ? 'https://api.whatsapp.com/send?phone=+254772348798' : `https://api.whatsapp.com/send?phone=${instructor.contacts[3].url}`}
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ textDecoration: 'none', color: 'yellowgreen' }}
+            >
+              <WhatsAppIcon /> <span style={{ fontSize: '0.5em' }}>Whatsapp</span>
+            </a>
+          </IconButton>
+          <IconButton
+            aria-label='share'
+            style={{ borderLeft: '1px solid white', borderRight: '1px solid white', borderRadius: '5px', borderRadius: '5px', padding: '5px', margin: 'auto 10px auto 0px' }}
           >
-            <LinkedInIcon style={{ color: 'blue' }} />
-          </a>
-        </IconButton>
-        <IconButton
-          disabled={
-            instructor.contacts[2].url === '' ||
-            instructor.contacts[2].url === null
-              ? true
-              : false
-          }
-        >
-          <a
-            href={instructor.contacts[2].url}
-            target='_blank'
-            rel='noopener noreferrer'
+            <a
+              href={instructor.contacts == undefined || instructor.contacts[0].url === null || instructor.contacts[0].url === '' ? 'https://www.linkedin.com/in/christopher-otieno-556779193/' : instructor.contacts[0].url}
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ textDecoration: 'none', color: 'white' }}
+            >
+              <LinkedInIcon /> <span style={{ fontSize: '0.5em' }}>LinkedIn</span>
+            </a>
+          </IconButton>
+          <IconButton
+            style={{ borderLeft: '1px solid #ff8c00', borderRight: '1px solid #ff8c00', borderRadius: '5px', borderRadius: '5px', padding: '5px', margin: 'auto 10px auto 0px' }}
           >
-            <ConnectWithoutContactIcon />
-          </a>
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label='show more'
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
+            <a
+              href={instructor.contacts == undefined || instructor.contacts[2].url === null || instructor.contacts[2].url === '' ? 'https://archademiks.slack.com/archives/C03BAMJV0JJ' : instructor.contacts[2].url}
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ textDecoration: 'none', color: '#ff8c00' }}
+            >
+              <ConnectWithoutContactIcon /> <span style={{ fontSize: '0.5em' }}>Slack</span>
+            </a>
+          </IconButton>
+        </div>
       </CardActions>
-      <Collapse in={expanded} timeout={'auto'} unmountOnExit>
-        <CardContent>
-          <Grid container justifyContent={'center'}>
-            {/* <Grid item xs={12}>
-              <Typography variant='h6'>Education History</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant='h6'>Work Experience</Typography>
-            </Grid> */}
-          </Grid>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
